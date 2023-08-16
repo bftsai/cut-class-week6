1f336cd83ae3a5316a807e8d9c0a6ce35ced40e3
bf_tsai <tsai19911026@gmail.com>
2023-08-16 12:00:33 +0800

    Updates

---
+++ assets/index-1c906b85.js
@@ -0,0 +1,1 @@
+import"./main-b95b9316.js";/* empty css                    */$(document).ready(function(){new Swiper(".swiper",{direction:"horizontal",speed:3e3,loop:!0,slidesPerView:"2",allowTouchMove:!1,spaceBetween:0,autoplay:{delay:0,disableOnInteraction:!1},effect:"slide",pagination:{el:".swiper-pagination",clickable:!1}})});
