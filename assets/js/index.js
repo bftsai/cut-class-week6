96a51eb32d19b0489f72ddbf1e83b6cca1d804b9
bf_tsai <tsai19911026@gmail.com>
2023-08-19 02:00:48 +0800

    finished-3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,4 @@
 $(document).ready(function () {
-    //swiper banner
-    // const swiper = new Swiper('#swiperBanner', {
-    //     // Optional parameters
-    //     direction: 'vertical',
-    //     loop: true,
-      
-    //     // If we need pagination
-    //     pagination: {
-    //       el: '.swiper-pagination',
-    //     },
-      
-    //     // Navigation arrows
-    //     navigation: {
-    //       nextEl: '.swiper-button-next',
-    //       prevEl: '.swiper-button-prev',
-    //     },
-      
-    //     // And if we need scrollbar
-    //     scrollbar: {
-    //       el: '.swiper-scrollbar',
-    //     },
-    //   });
     //swiper albums
     const swiper = new Swiper('.swiper', {
         direction: 'horizontal',
