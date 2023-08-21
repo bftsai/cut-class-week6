a8be543bfe1fa72ad893d41b1162b63fe7039ddc
bf_tsai <tsai19911026@gmail.com>
2023-08-21 10:54:50 +0800

    add login form validation

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './assets/scss/all.scss';
-// import 'bootstrap/dist/js/bootstrap.min.js';
-import './node_modules/bootstrap/dist/js/bootstrap.bundle';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+//import 'bootstrap/dist/js/bootstrap.min.js';
+//import './node_modules/bootstrap/dist/js/bootstrap.bundle';
+import './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 //tooltip
